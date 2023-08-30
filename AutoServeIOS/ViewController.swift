edf7f1c1f4fe9759a317f3b482fbc543a457944b
Sergio <spinfinite07@gmail.com>
2023-08-29 23:35:45 -0600

    Initial Commit

---
+++ AutoServeIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoServeIOS
+//
+//  Created by Dad's 6,1 on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
